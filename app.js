ab34373bcf32ebf8b90d8256a356356b72415fdb
jhcode001 <jhefne916@west-mec.org>
2026-09-25 08:56:33 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+// import Konva from 'konva';
+
+/* const container = document.getElementById("mainBody");
+
+const stage = new Konva.Stage({
+  container: 'mainBody',
+  width: container.clientWidth,
+  height: container.clientHeight,
+});
+
+const layer = new Konva.Layer();
+
+stage.add(layer);
+
+let circleProperties = {
+  x: 400,
+  y: 300,
+  radius: 50,
+  fill: 'red',
+  draggable: true
+}
+
+const circle = new Konva.Circle(circleProperties);
+
+const imageObj = new Image();
+imageObj.onload = function () {
+  const thing = new Konva.Image({
+    x:500,
+    y:500,
+    image: imageObj,
+    width: 1469,
+    height: 1008,
+    z: 1,
+
+  });
+
+    layer.add(thing);
+};
+
+imageObj.src = '..//assets/images/starting-page.jpg';
+layer.add(circle);
+
+let html = ``;
+
+html=`
+        <button id="startGame" class="start-game-btn">Start Game</button>
+        <button id="LoadSaves" class="Load-gamesaves-btn">Load Saves</button>`;
+
+container.innerHTML += html; */
